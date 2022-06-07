615e21f9da1c2b10bede94de0599a6807d0192a8
PY44N <pyan4444@gmail.com>
2022-06-07 19:15:51 -0400

    Added cli args and a clean thing

--- build.py
+++ build.py
@@ -1,5 +1,7 @@
 import os
 from sys import platform
+import argparse
+import multiprocessing
 
 # Settings
 CC = "gcc"  # The compiler
@@ -11,6 +13,19 @@ NAME = "out"  # Executable name
 SRCDIR = "src"  # Source directory
 
 # Source
+
+parser = argparse.ArgumentParser(
+    description='A light-weight build system written in python')
+
+parser.add_argument("-j", "--jobs", type=int,
+                    help="The number of jobs [default: %(default)s on your machine]", default=multiprocessing.cpu_count())
+
+parser.add_argument("-c", "--clean", action='store_true',
+                    help="Clean up object files after build")
+
+args = parser.parse_args()
+
+jobCount = args.jobs
 
 if platform == "win32":
     NAME += ".exe"
@@ -33,3 +48,13 @@ objectString = " ".join(objects)
 cmd = f"{CC} -o {NAME} {objectString} {LINKFLAGS}"
 print(cmd)
 os.system(cmd)
+
+if args.clean:
+    for subdir, dirs, files in os.walk(SRCDIR):
+        for file in files:
+            if ".o" not in file:
+                continue
+
+            file = os.path.join(subdir, file)
+            print(f"Removing {file}")
+            os.remove(file)
